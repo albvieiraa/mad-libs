32441f7f4629b76090367fa0f23f00c24322ce4f
Mary <maryllianbackup@gmail.com>
2023-11-30 15:02:11 -0300

    uptade

--- madlibs.py
+++ madlibs.py
@@ -6,7 +6,7 @@ adjetivo = input(str('Adjetivo: '))
 objeto_magico = input(str('Objeto Mágico: '))
 
 
-madlib = (f"Um dia, {nome} decidiu fazer uma viagem para {lugar}. No caminho, encontrou um {animal} muito {adjetivo}. Surpeendemente, o animal começou a falar e disse: 'Oi, {nome}! Estava esperando por você. Eu sou o guardião secreto de {lugar} e posso conceder a você um desejo mágico.' \
+madlibs = (f"Um dia, {nome} decidiu fazer uma viagem para {lugar}. No caminho, encontrou um {animal} muito {adjetivo}. Surpeendemente, o animal começou a falar e disse: 'Oi, {nome}! Estava esperando por você. Eu sou o guardião secreto de {lugar} e posso conceder a você um desejo mágico.' \
 {nome}, surpreso, decidiu pedir um(a) {objeto_magico}. O animal sorriu e disse: 'Seu desejo é uma ordem!' Num piscar de olhos, {nome} segurava o(a) {objeto_magico} nas mãos.")
 
-print(madlib)
+print(madlibs)
