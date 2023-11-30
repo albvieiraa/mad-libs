82972d5f080c8d929b1926e9f51573ab37d73830
Mary <maryllianbackup@gmail.com>
2023-11-30 14:50:36 -0300

    declaração das variáveis

--- madlibs.py
+++ madlibs.py
@@ -1,4 +1,9 @@
 #Concatenação de strings
+nome = input(str('Nome: '))
+lugar = input(str('Lugar: '))
+animal = input(str('Animal: '))
+adjetivo = input(str('Adjetivo: '))
+objeto_magico = input(str('Objeto Mágico: '))
 
 
 madlib = (f"Um dia, {nome} decidiu fazer uma viagem para {lugar}. No caminho, encontrou um {animal} muito {adjetivo}. Surpeendemente, o animal começou a falar e disse: 'Oi, {nome}! Estava esperando por você. Eu sou o guardião secreto de {lugar} e posso conceder a você um desejo mágico.' \
