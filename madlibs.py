4d02dda219a6b63b860bf09ec01e4fe5cf428e8b
Mary <maryllianbackup@gmail.com>
2023-11-30 14:48:01 -0300

    texto do madlibs

--- madlibs.py
+++ madlibs.py
@@ -1,3 +1,5 @@
 #Concatenação de strings
-youtuber = ""
-print(f"Subscribe to {youtuber}")
+
+
+madlib = (f"Um dia, {nome} decidiu fazer uma viagem para {lugar}. No caminho, encontrou um {animal} muito {adjetivo}. Surpeendemente, o animal começou a falar e disse: 'Oi, {nome}! Estava esperando por você. Eu sou o guardião secreto de {lugar} e posso conceder a você um desejo mágico.' \
+{nome}, surpreso, decidiu pedir um(a) {objeto_magico}. O animal sorriu e disse: 'Seu desejo é uma ordem!' Num piscar de olhos, {nome} segurava o(a) {objeto_magico} nas mãos.")
